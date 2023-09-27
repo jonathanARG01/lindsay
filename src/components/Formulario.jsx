fa4c9f535f2b1437b751df41b844cf166ea479bb
Jonathan Rojas <jonathansw01@gmail.com>
2023-09-27 18:26:40 -0300

    Formulario react-hook-form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,47 +1,80 @@
-import React, { useState } from 'react'
-
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 
 const Formulario = () => {
-
-    const [name, setName]       = useState('');
-    const [email, setEmail]     = useState('');
-    const [message, setMessage] = useState('');
     
-    const handleForm = ( event ) => {
-        event.preventDefault();
-        console.log( event )
+    // register: Registrar y vincular campos al formulario
+    // formState: Estados del formulario (errores)
+    // handleSubmit: Metodo cuando se envia el formulario
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = ( data ) => {
+        console.log( data );
     }
 
     return (
     
         <>
         
-            <form onSubmit={ handleForm }>
+            <form onSubmit={ handleSubmit( onSubmit ) }>
 
                 <div className="mb-4">
+                    
                     <label className="form-label">
                         <img src="icon-user.svg" alt="icon"/>
                         Nombre
                     </label>
-                    <input type="text" name="name" className="form-control" id="inputNombre" placeholder="name@example.com"></input>
+                    
+                    <input
+                        type="text"
+                        name="name"
+                        className="form-control"
+                        placeholder="name@example.com"
+                        { ...register('name', { required: true }) } />
+
+                    { errors.name?.type === 'required' && <span className="error">El nombre es requerido</span> }
+
                 </div>
 
+
                 <div className="mb-4">
+                    
                     <label className="form-label">
                         <img src="icon-email.svg" alt="icon"/>
                         Correo Electrónico
-                        </label>
-                    <input type="email" name="email" className="form-control" id="inputEmail" placeholder="Ej: correo@electronico.com"></input>
+                    </label>
+                    
+                    <input
+                        type="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Ej: correo@electronico.com"
+                        { ...register('email', { required: true }) } />
+
+                        { errors.email?.type === 'required' && <span className="error">El correo es requerido</span> }
+                
                 </div>
 
+
                 <div className="mb-4">
+
                     <label className="form-label">
                         <img src="icon-pencil.svg" alt="icon"/>
                         Mensaje
                     </label>
-                    <textarea name="message" className="form-control" id="inputMensaje" rows="3" placeholder='Escribe aquí tu mensaje'></textarea>
+
+                    <textarea
+                        name="message"
+                        className="form-control"
+                        rows="3"
+                        placeholder='Escribe aquí tu mensaje'
+                        { ...register('message') } >
+                    </textarea>
+
                 </div>
+
 
                 <div className="text-center">
                     <button type='submit' className='btn'>¡Contáctanos hoy mismo!</button>
